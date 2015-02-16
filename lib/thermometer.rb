639b437ab6e557e8c157ea16f00045b3ee74377e
Anthony Carlos <anthony@carlosfamily.com>
2015-02-16 10:00:07 -0500

    Changed regex to allow for negative temperatures.

--- lib/thermometer.rb
+++ lib/thermometer.rb
@@ -1,7 +1,7 @@
 class Thermometer
   def self.read
     reading = `cat /sys/bus/w1/devices/28-000003e8346c/w1_slave`
-    r = /t=([[:digit:]]+)/
+    r = /t=([-]?[[:digit:]]+)/
     c = (r.match(reading)[1].to_f) / 1000
     f = (1.8 * c) + 32
   end
